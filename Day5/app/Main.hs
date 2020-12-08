8c0dae5fb8f3e0ba14671ff803a698aa68ee6a4d
Stoephu <christoph.straehl@gmx.ch>
2020-12-08 17:07:11 +0100

    End of day 5 struggle

--- Day5/app/Main.hs
+++ Day5/app/Main.hs
@@ -21,13 +21,22 @@ main = do
     print maxId
     -- part 2
     let minId = minimum ids
-    let triples = zip3 ids robin colbin
+    let missingFront = [0,1.. minId-1]
+    let missingBack = [maxId +1 .. 1023]
+    ---let result = 1024 - (sum  (map (+1) (missingFront ++ ids ++ missingBack)) `mod` 1024) - 1
+    let result = 1023 - (sum  ( (missingFront ++ ids ++ missingBack)) `mod` 1023 ) 
+    print (List.sort $ missingFront ++ ids ++ missingBack)
+    print minId
+    print maxId
+    print result 
+    print (result `elem` ids)
+    {-let triples = zip3 ids robin colbin
     print triples
     let sorted = List.sort triples
     print sorted
     let leftOver = (foldl1 listXor robin, foldl1 listXor colbin)
     print leftOver
-    print ((\(a,b) -> a*8 + b) (getInt 7 (fst leftOver), getInt 3 (snd leftOver)))
+    print ((\(a,b) -> a*8 + b) (getInt 7 (fst leftOver), getInt 3 (snd leftOver)))-}
     hClose handle
 
 toBinary (b1,b2) = (binify b1 , binify b2)
@@ -46,10 +55,11 @@ listXor as bs = [a /= b | (a,b) <- zip as bs]
 getLeftOver :: [Bool] -> [[Bool]] -> [Bool]
 getleftOver current [] = current
 getLeftOver current (l:ls) = getLeftOver [b1 /= b2 | (b1,b2) <- zip current l] ls
-getLeftOver [] _ = error "Current is empty"-}
+getLeftOver [] _ = error "Current is empty"
 
 generateSeats start stop 
     | start == stop = []
     | otherwise = start1 : generateSeats start1 stop
         where start1 = add1 start
               add1 = foldr (\x acc -> (x /= head acc) : acc) [True] 
+              -}
