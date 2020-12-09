9b830d87f80eca9796e2ee161e48e853ad2ca9c7
Stoephu <christoph.straehl@gmx.ch>
2020-12-09 15:58:40 +0100

    Day 7 Part 1 done

--- Day7/app/Main.hs
+++ Day7/app/Main.hs
@@ -8,14 +8,17 @@ import Data.Text(pack, unpack, replace)
 
 main :: IO ()
 main = do
-    handle <- openFile "test.txt" ReadMode
+    handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let lines = splitOn "\n"  contents
     let personalBag = "shiny gold"
     let rulesString = map (rplSubStr " bags contain " " " . rplSubStr " bag, " " " . rplSubStr " bags, " " " . rplSubStr " bags." "" . rplSubStr " bag." "") lines
     let rulesList = map (splitOn " ") rulesString
     let rulesTuples = makeTupleList rulesList
-    print rulesTuples
+    --print rulesTuples
+    let overBags = getAllOverBags [personalBag] rulesTuples
+    --print overBags
+    print (Set.size overBags)
     hClose handle
 
 rplSubStr match replacement = unpack . replace (pack match) (pack replacement) . pack 
@@ -27,3 +30,8 @@ getRest :: [String] -> [(Int,String)]
 getRest ["no","other"] = []
 getRest [] = []
 getRest (num:a:b:rest) = (read num, a ++ " " ++ b) : getRest rest
+
+getAllOverBags :: [String] -> [(String, [(Int,String)])] -> Set.Set String
+getAllOverBags [] _ = Set.empty :: Set.Set String
+getAllOverBags (bagName:rest) rules = Set.union (Set.fromList hits) (getAllOverBags (rest ++ hits) rules)
+    where hits = [overBag | (overBag, tuples) <- rules, bagName `elem` [bag | (_,bag) <- tuples ]]
