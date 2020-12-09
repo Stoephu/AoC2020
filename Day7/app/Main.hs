95b81b5d9a5d1e15bc28093e703c1ef31da49127
Stoephu <christoph.straehl@gmx.ch>
2020-12-09 12:25:53 +0100

    Day7 struggles

--- Day7/app/Main.hs
+++ Day7/app/Main.hs
@@ -3,12 +3,27 @@ module Main where
 import System.IO
 import Data.List.Split
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.Text(pack, unpack, replace)
 
 main :: IO ()
 main = do
-    handle <- openFile "input.txt" ReadMode
+    handle <- openFile "test.txt" ReadMode
     contents <- hGetContents handle
-    let paragraphs = splitOn "\n\n"  contents
-    let splittedparas = map ( splitOn "\n") paragraphs
-
+    let lines = splitOn "\n"  contents
+    let personalBag = "shiny gold"
+    let rulesString = map (rplSubStr " bags contain " " " . rplSubStr " bag, " " " . rplSubStr " bags, " " " . rplSubStr " bags." "" . rplSubStr " bag." "") lines
+    let rulesList = map (splitOn " ") rulesString
+    let rulesTuples = makeTupleList rulesList
+    print rulesTuples
     hClose handle
+
+rplSubStr match replacement = unpack . replace (pack match) (pack replacement) . pack 
+
+makeTupleList = map getTuple 
+
+getTuple (a:b:rest) = (a ++ " " ++ b, getRest rest)
+getRest :: [String] -> [(Int,String)]
+getRest ["no","other"] = []
+getRest [] = []
+getRest (num:a:b:rest) = (read num, a ++ " " ++ b) : getRest rest
