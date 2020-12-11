abd5044a81803000deb29c48f758f998f20c78ec
Stoephu <christoph.straehl@gmx.ch>
2020-12-11 22:38:07 +0100

    part1 day1'

--- Day10/app/Main.hs
+++ Day10/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import System.IO
 import Data.List.Split
+import Data.List
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Data.Maybe
@@ -12,4 +13,20 @@ main = do
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let lines = splitOn "\n"  contents
+    let numbers = sort (map read lines)
+    let appNumbers = 0:numbers ++ [(maximum numbers) + 3]
+    print appNumbers
+    let ones = count1 appNumbers 
+    let threes = count3 appNumbers
+    print ones 
+    print threes
+    print (ones * threes)
     hClose handle
+
+count1 :: [Int] -> Int
+count1 [a,b] = if (b-a) == 1 then 1 else 0
+count1 (a:b:ls) = if (b-a) == 1 then 1 + (count1 (b:ls)) else count1 (b:ls)
+
+count3 :: [Int] -> Int
+count3 [a,b] = if (b-a) == 3 then 1 else 0
+count3 (a:b:ls) = if (b-a) == 3 then 1 + (count3 (b:ls)) else count3 (b:ls)
